b1616fba1e6151a8b9f19aacdec3f78ed171fcb5
Piyamin <piyaminjaima42@gmail.com>
2019-09-21 12:31:27 +0700

    Unit1

---
+++ 2-if-for.js
@@ -0,0 +1,38 @@
+// const a = "a";
+// if (a == "a") {
+//     console.log("It's a");
+// } else {
+//     console.log("It' not a");
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// Console.log(i);
+
+// let i = 0;
+// while (i < 10) {
+//     console.log(i);
+//     i++;
+// }
+// let i = 0;
+// let sum = 0;
+// while (i < 1000) {
+//     sum = sum + i;
+//     i = i++;
+// }
+// console.log(sum);
+
+let a = 1; // int a =1;
+let b = 1.1; // float b =1.1;
+let c = "hello";
+
+let h = [];
+let student = {
+    id: "61105631",
+    "name - Lastname": "Piyamin",
+    faculty: "Science"
+};
+console.log(student["name - Lastname"]);
+student["name - Lastname"] = "Ging";
+console.log(student["name - Lastname"]);
